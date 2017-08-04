9d486672f47730f0007c22aeed89b76a6e10bfa7
Jenny Robertson <jenny.robertson@bbc.co.uk>
2017-08-04 12:52:28 +0100

    renamed

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,25 +1,23 @@
 javascript:(function() {
 
-labelDiv = document.getElementById('window-width-label');
-if (labelDiv) {
-    labelDiv.remove();
+preexisitingLabel = document.getElementById('window-width-label');
+if (preexisitingLabel) {
+    preexisitingLabel.remove();
     return;
 }
-
 
 var label = document.createElement('div');
 label.setAttribute('id', 'window-width-label');
 label.setAttribute('style', 'background-color: rgba(255, 255, 255, 0.9);padding: 5px;position: fixed;left: 0px;top: 0px; z-index:1000; font-size: 18px; font-family: "Courier", Courier, "Lucida Sans Typewriter", "Lucida Typewriter", monospace;');
-label.innerHTML = 'XXL';
-
 document.body.appendChild(label);
-window.onresize = resized;
+
 resized();
+window.onresize = resized;
 
 function resized () {
 	width=window.innerWidth;
   breakPoint = getBreakpoint(width);
-	label.innerHTML = width + "px  "+ breakPoint;
+	label.innerHTML = width + "px  "+  breakPoint;
 }
 
 function getBreakpoint(width){
