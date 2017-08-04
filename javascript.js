bd2edb008f70e40e87ec9e716026ded0ef996370
Jenny Robertson <jenny.robertson@bbc.co.uk>
2017-08-04 12:19:24 +0100

    Improve appearance

--- javascript.js
+++ javascript.js
@@ -1,6 +1,15 @@
+javascript:(function() {
+
+labelDiv = document.getElementById('window-width-label');
+if (labelDiv) {
+    labelDiv.remove();
+    return;
+}
+
+
 var label = document.createElement('div');
 label.setAttribute('id', 'window-width-label');
-label.setAttribute('style', 'background-color: grey;padding: 10px;position: fixed;right: 0px;top: 0px; z-index:10000000');
+label.setAttribute('style', 'background-color: rgba(255, 255, 255, 0.9);padding: 5px;position: fixed;left: 0px;top: 0px; z-index:1000; font-size: 18px; font-family: "Courier", Courier, "Lucida Sans Typewriter", "Lucida Typewriter", monospace;');
 label.innerHTML = 'XXL';
 
 document.body.appendChild(label);
@@ -14,10 +23,12 @@ function resized () {
 }
 
 function getBreakpoint(width){
-if(width < 400) return 'XS';
+	if(width < 400) return 'XS';
   else if(width < 600) return 'S';
   else if(width < 900) return 'M';
   else if(width < 1008) return 'L';
   else if(width < 1280) return 'XL';
   else return 'XXL';
 }
+
+})();
